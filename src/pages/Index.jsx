b476ea3e20ca07387cb51a500f90c3aba6432ffb
lucho <luisrak8@gmail.com>
2022-10-30 18:36:35 +0100

    feat:codificamos banner

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "../component/header/Header";
+import Banner from "../component/banner/Banner";
 
 const Index = () => {
-  return <Header />;
+  return (
+    <section>
+      <Header />
+      <Banner />
+    </section>
+  );
 };
 
 export default Index;
