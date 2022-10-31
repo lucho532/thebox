8166597ab048be715c08abd36ac2546a577ed6b6
lucho <luisrak8@gmail.com>
2022-10-31 18:22:11 +0100

    feat: componente reputation

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../component/header/Header";
 import Banner from "../component/banner/Banner";
+import Reputation from "../component/reputation/Reputation";
 
 const Index = () => {
   return (
     <section>
       <Header />
       <Banner />
+      <Reputation />
     </section>
   );
 };
